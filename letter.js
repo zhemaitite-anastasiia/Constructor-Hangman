6a28b01e04f1b7a4043ac048648f8b823efb40ed
AnastasiiaZhemaitite <a.zhemaitite@gmail.com>
2018-02-12 14:52:15 -0600

    Changes to comments and console log corrections

--- letter.js
+++ letter.js
@@ -1,16 +1,15 @@
-console.log('inside of letter.js')
 exports.letter = letter;
 
 function letter(value) {
-	this.value = value;
-	this.show = false;
-	if (this.value == ' ')
-		this.show = true;
+    this.value = value;
+    this.show = false;
+    if (this.value == ' ')
+        this.show = true;
 }
 
-letter.prototype.printInfo = function(){
-	if (this.show) {
-		return this.value;
-	}
-	return "_";
+letter.prototype.printInfo = function() {
+    if (this.show) {
+        return this.value;
+    }
+    return "_";
 }
--- main.js
+++ main.js
@@ -6,41 +6,37 @@ var validator = require('validator');
 var randomWord = game.randomWord;
 var letterGuess;
 exports.letter;
-console.log('inside of main.js')
+
 
 var myWord = new wordCons.wordCons(game.randomWord);
 var maxGuesses = 10;
 
-function takeGuess(){
-	console.log(myWord.toString());
-	if (myWord.guessesMade.length >= maxGuesses){
-		console.log("No more guessses left");
-		return;
-	}
-	inquirer.prompt([{
-		name: 'letter',
-		type: 'text',
-		message: 'Enter letter: ',
-		//validate: function{}
-	}]).then(function(letterInput){ 
-		var letter = letterInput.letter;
-		myWord.findLetter(letter);
-		if (myWord.isComplete()){
-			console.log("It was " + myWord.toString() + "!");
-			return;
-		}
-		console.log('-----------\n');
-		console.log('You have ' + (maxGuesses - myWord.guessesMade.length) + ' guesses left.')
+function takeGuess() {
+    console.log(myWord.toString());
+    if (myWord.guessesMade.length >= maxGuesses) {
+        console.log("No more guessses left");
+        return;
+    }
+    inquirer.prompt([{
+        name: 'letter',
+        type: 'text',
+        message: 'Guess a letter: ',
 
-		//console.log(letterInput)
-		//if(validator.isAlpha(letterInput.letter) && letterInput.letter.length === 1){
-			console.log('its just one letter!')
-			takeGuess();
-		}) //else {
-			//console.log('please, one letter at a time')
-			//takeGuess();
-		
-	
+    }]).then(function(letterInput) {
+        var letter = letterInput.letter;
+        myWord.findLetter(letter);
+        if (myWord.isComplete()) {
+            console.log("You are right! It was " + myWord.toString() + "!");
+            return;
+        }
+        console.log('-------------------------------------------------------------------------\n');
+        console.log('You have ' + (maxGuesses - myWord.guessesMade.length) + ' guesses left.')
+
+
+        takeGuess();
+    })
+
+
 }
 
 takeGuess();
